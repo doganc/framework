5c94987b56406e8123ca90e937e541ed19bbf280
mehdy-karimpour <mehdy.karimpour@signumsoftware.com>
2024-03-28 23:01:17 +0330

    fix FieldReference in replacing ToString (e.g. UserEntity)

--- Signum.Engine.Extensions/Cache/CachedTable.cs
+++ Signum.Engine.Extensions/Cache/CachedTable.cs
@@ -776,7 +776,7 @@ class CachedLiteTable<T> : CachedTableBase where T : Entity
 
         private IColumn GetColumn(Field field)
         {
-            if (field is FieldPrimaryKey || field is FieldValue || field is FieldTicks)
+            if (field is FieldPrimaryKey || field is FieldValue || field is FieldTicks || field is FieldReference)
                 return (IColumn)field;
 
             throw new InvalidOperationException("{0} not supported when caching the ToString for a Lite of a transacional entity ({1})".FormatWith(field.GetType().TypeName(), this.table.Type.TypeName()));
